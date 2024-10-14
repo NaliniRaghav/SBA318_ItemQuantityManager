3d59bdfd884e8ea55550478a567314862eed432b
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-10-14 16:29:36 -0400

    third

--- data/categories.js
+++ data/categories.js
@@ -1,6 +1,8 @@
 export const categories = [
     { id: 1, name: "Fruits" },
     { id: 2, name: "Vegetables" },
-    { id: 3, name: "Flowers"}
+    { id: 3, name: "Flowers"},
+    { id: 4, name: "Diary"}
+
   ];
   
--- data/items.js
+++ data/items.js
@@ -1,7 +1,8 @@
 export const items = [
     { id: 1, name: "Apples", quantity: 50, category: 1, supplier: 1 },
     { id: 2, name: "Bananas", quantity: 30, category: 1, supplier: 2 },
-    { id: 2, name: "Orange", quantity: 100, category: 1, supplier: 3 }
+    { id: 3, name: "Orange", quantity: 100, category: 1, supplier: 3 },
+    { id: 4, name: "Milk", quantity: 100, category: 1, supplier: 3 }
 
   ];
   
